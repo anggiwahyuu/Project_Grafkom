543d4997e0bc76b01edadbc376c83b5c3d45948c
anggiwahyuu <anggiwahyu543@gmail.com>
2020-12-01 08:23:52 +0700

    update program

--- Project_Grafkom/main.cpp
+++ Project_Grafkom/main.cpp
@@ -4,6 +4,7 @@
 #include <GL/glut.h>
 #include <time.h>
 #include <iostream>
+#include <math.h>
 
 bool lompat = true;
 bool movee = true;
@@ -17,9 +18,12 @@ float posisiY[2] = {1.6, 4.6};
 float x[2] = {0, 3.5};
 float y[2] = {0, 3.5};
 
+//membuat collider
 void colliderPersegi()
 {
     glPushMatrix();
+    glTranslated(40,10,0);
+    glScalef(3,5,0);
     glBegin(GL_POLYGON);
     glColor4f(1,1,1,0);
     glVertex2f(posisiX[0], posisiY[0]); //H6
@@ -30,11 +34,14 @@ void colliderPersegi()
     glPopMatrix();
 }
 
+//membuat karakter sapi
 void sapi()
 {
     colliderPersegi();
 
     glPushMatrix();
+    glTranslated(40,10,0);
+    glScalef(3,5,0);
     glTranslated(movement,jump,0);
 //bagian kepala sapi
     //telinga kiri sapi
@@ -535,10 +542,12 @@ void sapi()
     glPopMatrix();
 }
 
+//membuat rintangan berupa box
 void box()
 {
     glPushMatrix();
-    glTranslated(-5,0,0);
+    glTranslated(-10,0,0);
+    glScalef(3,5,0);
     glTranslated(bot,0,0);
     glBegin(GL_POLYGON);
     glColor3f(0,0,0);
@@ -550,61 +559,334 @@ void box()
     glPopMatrix();
 }
 
+//membuat background
+void background()
+{
+    glMatrixMode(GL_MODELVIEW);
+
+    //Bagian tanah berwarna coklat
+    glColor3ub(169,127,91);
+    glBegin(GL_QUADS);
+    glVertex2i(0,0);
+    glVertex2i(0,10);
+    glVertex2i(124,10);
+    glVertex2i(124,0);
+    glEnd();
+    glFlush();
+
+    //Bagian rumput yang tampak samping
+    glColor3ub(61,204,84);
+    glBegin(GL_QUADS);
+    glVertex2i(0,10);
+    glVertex2i(0,13);
+    glVertex2i(124,13);
+    glVertex2i(124,10);
+    glEnd();
+    glFlush();
+
+    //Bagian jalan untuk sapi
+    glColor3ub(167,235,86);
+    glBegin(GL_QUADS);
+    glVertex2i(0,13);
+    glVertex2i(0,29);
+    glVertex2i(124,29);
+    glVertex2i(124,13);
+    glEnd();
+    glFlush();
+
+    //Bagian jalan (rumput) untuk sapi
+    glColor3ub(167,235,86);
+    glBegin(GL_QUADS);
+    glVertex2i(0,13);
+    glVertex2i(0,29);
+    glVertex2i(124,29);
+    glVertex2i(124,13);
+    glEnd();
+    glFlush();
+
+    //Bagian lantai rumah
+    glColor3ub(160,135,104);
+    glBegin(GL_QUADS);
+    glVertex2i(5.54,29);
+    glVertex2i(5.54,31.5);
+    glVertex2i(42,31.5);
+    glVertex2i(42,29);
+    glEnd();
+    glFlush();
+
+    //Bagian bagan rumah
+    glColor3ub(180,152,113);
+    glBegin(GL_QUADS);
+    glVertex2i(6.7,31.5);
+    glVertex2i(6.7,60);
+    glVertex2i(41.7,60);
+    glVertex2i(41.7,31.5);
+    glEnd();
+    glFlush();
+
+    //Bagian garis kecil diantara bagan rumah dan atap
+    glColor3ub(160,135,104);
+    glBegin(GL_QUADS);
+    glVertex2i(6.7,60);
+    glVertex2i(6.7,62);
+    glVertex2i(41.7,62);
+    glVertex2i(41.7,60);
+    glEnd();
+    glFlush();
+
+    //Bagian pintu rumah di bagan
+    glColor3ub(86,74,58);
+    glBegin(GL_QUADS);
+    glVertex2i(20,31.5);
+    glVertex2i(20,50);
+    glVertex2i(27,50);
+    glVertex2i(27,31.5);
+    glEnd();
+    glFlush();
+
+    //garis pintu kiri
+    glColor3ub(239,229,217);
+    glBegin(GL_QUADS);
+    glVertex2i(18,31.5);
+    glVertex2i(18,60);
+    glVertex2i(19,60);
+    glVertex2i(19,31.5);
+    glEnd();
+    glFlush();
+
+    //garis pintu kanan
+    glColor3ub(239,229,217);
+    glBegin(GL_QUADS);
+    glVertex2i(28,31.5);
+    glVertex2i(28,60);
+    glVertex2i(29,60);
+    glVertex2i(29,31.5);
+    glEnd();
+    glFlush();
+
+    //garis pintu atas
+    glColor3ub(239,229,217);
+    glBegin(GL_QUADS);
+    glVertex2i(19,60);
+    glVertex2i(19,62);
+    glVertex2i(28,62);
+    glVertex2i(28,60);
+    glEnd();
+    glFlush();
+
+    //jendela kiri
+    glColor3ub(86,74,58);
+    glBegin(GL_QUADS);
+    glVertex2i(9,40);
+    glVertex2i(9,50);
+    glVertex2i(15,50);
+    glVertex2i(15,40);
+    glEnd();
+    glFlush();
+
+     //jendela kanan
+    glColor3ub(86,74,58);
+    glBegin(GL_QUADS);
+    glVertex2i(32,40);
+    glVertex2i(32,50);
+    glVertex2i(38,50);
+    glVertex2i(38,40);
+    glEnd();
+    glFlush();
+
+    //cerobong asap
+    glColor3ub(160,135,104);
+    glBegin(GL_QUADS);
+    glVertex2i(8,62);
+    glVertex2i(8,81);
+    glVertex2i(12,81);
+    glVertex2i(12,62);
+    glEnd();
+    glFlush();
+
+    //atap 1
+    glColor3ub(231,214,194);
+    glBegin(GL_QUADS);
+    glVertex2i(5.54,62);
+    glVertex2i(5.54,64);
+    glVertex2i(42,64);
+    glVertex2i(42,62);
+    glEnd();
+    glFlush();
+
+    //atap 2
+    glColor3ub(231,214,194);
+    glBegin(GL_QUADS);
+    glVertex2i(6,64);
+    glVertex2i(6,66);
+    glVertex2i(41,66);
+    glVertex2i(41,64);
+    glEnd();
+    glFlush();
+
+    //atap 3
+    glColor3ub(231,214,194);
+    glBegin(GL_QUADS);
+    glVertex2i(7,66);
+    glVertex2i(7,68);
+    glVertex2i(40,68);
+    glVertex2i(40,66);
+    glEnd();
+    glFlush();
+
+    //atap 4
+    glColor3ub(231,214,194);
+    glBegin(GL_QUADS);
+    glVertex2i(8,68);
+    glVertex2i(8,70);
+    glVertex2i(39,70);
+    glVertex2i(39,68);
+    glEnd();
+    glFlush();
+
+    //atap 5
+    glColor3ub(231,214,194);
+    glBegin(GL_QUADS);
+    glVertex2i(9,70);
+    glVertex2i(9,72);
+    glVertex2i(38,72);
+    glVertex2i(38,70);
+    glEnd();
+    glFlush();
+
+    //atap 6
+    glColor3ub(231,214,194);
+    glBegin(GL_QUADS);
+    glVertex2i(10,72);
+    glVertex2i(10,74);
+    glVertex2i(37,74);
+    glVertex2i(37,72);
+    glEnd();
+    glFlush();
+
+    //atap 6
+    glColor3ub(231,214,194);
+    glBegin(GL_QUADS);
+    glVertex2i(11,74);
+    glVertex2i(11,76);
+    glVertex2i(36,76);
+    glVertex2i(36,74);
+    glEnd();
+    glFlush();
+
+    //atap 7
+    glColor3ub(231,214,194);
+    glBegin(GL_QUADS);
+    glVertex2i(12,76);
+    glVertex2i(12,78);
+    glVertex2i(35,78);
+    glVertex2i(35,76);
+    glEnd();
+    glFlush();
+
+    //atap 8
+    glColor3ub(231,214,194);
+    glBegin(GL_QUADS);
+    glVertex2i(13,78);
+    glVertex2i(13,80);
+    glVertex2i(34,80);
+    glVertex2i(34,78);
+    glEnd();
+    glFlush();
+
+    //atap 9
+    glColor3ub(231,214,194);
+    glBegin(GL_QUADS);
+    glVertex2i(14,80);
+    glVertex2i(14,82);
+    glVertex2i(33,82);
+    glVertex2i(33,80);
+    glEnd();
+    glFlush();
+
+    //atap 10
+    glColor3ub(231,214,194);
+    glBegin(GL_QUADS);
+    glVertex2i(15,82);
+    glVertex2i(15,84);
+    glVertex2i(32,84);
+    glVertex2i(32,82);
+    glEnd();
+    glFlush();
+}
+
+//looping pada box
 void displaybox()
 {
     float a = 0;
-    float b = 5;
+    float b = 70;
+    float c = 17;
     for (int i = 0; i<=100; i++){
         glPushMatrix();
-        glTranslated(a,0,0);
+        glTranslated(a,c,0);
         box();
         glPopMatrix();
         a -= b;
-        b += 5;
+        if (i%2 == 0){
+            c = 35;
+        }
+        else if (i%2 != 0){
+            c = 17;
+        }
     }
 }
 
+//movement pada sapi
 void characterController(int data){
     if(GetAsyncKeyState(VK_LEFT)){
-        if (posisiX[0] >= 0){
+        if (posisiX[0] >= -14){
             posisiX[0] -= 1.0f;
             posisiX[1] -= 1.0f;
             movement -= 1.0f;
         }
     } else if(GetAsyncKeyState(VK_RIGHT)){
-        if (posisiX[1] <= 30){
+        if (posisiX[1] <= 29){
             posisiX[0] += 1.0f;
             posisiX[1] += 1.0f;
             movement += 1.0f;
         }
     }
     if(GetAsyncKeyState(VK_UP)){
-        if (posisiY[1] <= 10){
+        if (posisiY[1] <= 8){
             posisiY[0] += 1.0f;
             posisiY[1] += 1.0f;
             jump += 1.0f;
         }
     } else if (lompat == true){
-        if (posisiY[0] >= 1){
+        if (posisiY[0] >= 2){
             posisiY[0] -= 1.0f;
             posisiY[1] -= 1.0f;
             jump -= 1.0f;
         }
     }
+
+    glutPostRedisplay();
+    glutTimerFunc(20,characterController,0);
+}
+
+//movement speed box
+void boxMove(int data)
+{
     if (movee == true){
         x[0] += 0.1f;
         x[1] += 0.1f;
         bot += 0.1f;
     }
     glutPostRedisplay();
-    glutTimerFunc(20,characterController,0);
+    glutTimerFunc(10,boxMove,0);
 }
 
 void displayMe()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    sapi();
+    background();
     displaybox();
+    sapi();
     glFlush();
     glutSwapBuffers();
 }
@@ -614,7 +896,7 @@ void myinit()
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glMatrixMode(GL_MODELVIEW);
-    glClearColor(1.0,1.0,0.0,1.0);
+    glClearColor(0.0,0.8,1.0,1.0);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
@@ -624,11 +906,12 @@ int main(int argc, char** argv)
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
     glutInitWindowPosition(0,0);
-    glutInitWindowSize(750,750);
+    glutInitWindowSize(1520,780);
     glutCreateWindow("Execute");
-    gluOrtho2D(0,30,0,30);
+    gluOrtho2D(0,124,0,124);
     glutDisplayFunc(displayMe);
     glutTimerFunc(1,characterController,0);
+    glutTimerFunc(1,boxMove,0);
     myinit();
     glutMainLoop();
     return 0;
